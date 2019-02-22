07cb83bd747876af3fdeee690e04105e115e9e0d
ktiagot <tiagocalutis@hotmail.com.br>
2019-02-22 19:46:29 -0300

    Iniciando

---
+++ src/a2202/Exercicio02_2.java
@@ -0,0 +1,25 @@
+package a2202;
+
+import java.util.Scanner;
+
+public class Exercicio02_2 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	}
+	//Somar um ponto para cada caractere de A que constar em B
+	public static int contaIguais(String p1, String p2) {
+		int pontos = 0;
+		return pontos;
+	}
+	//Somar dois pontos para cada caractere de P1 que constar em P2 e estiver na mesma proporção
+	public static int contaProporcao(String p1, String p2) {
+		int pontos = 0;
+		return pontos;
+	}
+	//Somar três pontos para cada caractere de A que constar em B e estiver na mesma posição
+	public static int contaPosicao(String p1, String p2) {
+		int pontos = 0;
+		return pontos;
+	}
+}
